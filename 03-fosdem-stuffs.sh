954cbeb8b4ea794fa07d3eb3646302eed24cf7e8
Tom Barbette <barbette@kth.se>
2021-01-22 15:19:24 +0100

    Test were re-run at 1.2Ghz, while only the last one should.
    Fixed now.

--- 03-fosdem-stuffs.sh
+++ 03-fosdem-stuffs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-npf-run fastclick --cluster client=nslrack17 dut=nslrack18 --test 03-fosdem-stuffs.npf --graph-filename 03-fosdem-stuffs/.svg $@ 
+npf-run fastclick --cluster client=nslrack17 dut=nslrack18 --test 03-fosdem-stuffs.npf --graph-filename 03-fosdem-stuffs-results/.svg $@ 
